03a4157e76c169323339d8e9d555286bd0fbf691
Soulou <leo@unbekandt.eu>
2015-01-04 20:43:43 +0100

    Allow OPTIONS for ember

--- cors_middleware.go
+++ cors_middleware.go
@@ -6,7 +6,7 @@ func CorsMiddleware(next HandlerFunc) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, vars map[string]string) error {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 		return next(w, r, vars)
 	}
 }
