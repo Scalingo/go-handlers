1f059f44fadd280ed7cbaba706e74339196cefea
Soulou <leo@unbekandt.eu>
2017-09-01 15:11:54 +0200

    Logging is now handled by Logrus only to have key-value logging, logger is to pass as argument of the middleware

--- logging_middleware.go
+++ logging_middleware.go
@@ -1,26 +1,52 @@
 package handlers
 
 import (
-	"log"
+	"context"
 	"net/http"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/negroni"
 )
 
 type LoggingMiddleware struct {
-	logger *log.Logger
+	logger logrus.FieldLogger
 }
 
-func NewLoggingMiddleware(logger *log.Logger) Middleware {
+func NewLoggingMiddleware(logger *logrus.Logger) Middleware {
 	m := &LoggingMiddleware{logger}
 	return m
 }
 
 func (l *LoggingMiddleware) Apply(next HandlerFunc) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, vars map[string]string) error {
+		logger := l.logger
 		before := time.Now()
-		l.logger.Printf("%v %v %v - %v", before, r.Method, r.URL, r.RemoteAddr)
+
+		id, ok := r.Context().Value("request_id").(string)
+		if ok {
+			logger = logger.WithField("request_id", id)
+		}
+
+		r = r.WithContext(context.WithValue(r.Context(), "logger", logger))
+
+		fields := logrus.Fields{
+			"method":     r.Method,
+			"path":       r.URL.String(),
+			"host":       r.Host,
+			"from":       r.RemoteAddr,
+			"protocol":   r.Proto,
+			"referer":    r.Referer(),
+			"user_agent": r.UserAgent(),
+		}
+		for k, v := range fields {
+			if len(v.(string)) == 0 {
+				delete(fields, k)
+			}
+		}
+		logger = logger.WithFields(fields)
+		logger.Info()
+
 		rw := negroni.NewResponseWriter(w)
 		err := next(rw, r, vars)
 		after := time.Now()
@@ -29,7 +55,13 @@ func (l *LoggingMiddleware) Apply(next HandlerFunc) HandlerFunc {
 		if status == 0 {
 			status = 200
 		}
-		l.logger.Printf("%v %v %v - %v - %d - %0.4f", after, r.Method, r.URL, r.RemoteAddr, status, after.Sub(before).Seconds())
+
+		logger.WithFields(logrus.Fields{
+			"status":   status,
+			"duration": after.Sub(before).Seconds(),
+			"bytes":    rw.Size(),
+		}).Info()
+
 		return err
 	}
 }
