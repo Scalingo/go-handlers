60df05a3b9a3e4043e405342331d349e18bb7a76
Soulou <leo@unbekandt.eu>
2018-01-07 20:04:17 +0100

    Fix specs as uuid.NewV4() api has changed

--- request_id_middleware.go
+++ request_id_middleware.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/satori/go.uuid"
@@ -11,7 +12,11 @@ func RequestIDMiddleware(next HandlerFunc) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, vars map[string]string) error {
 		id := r.Header.Get("X-Request-ID")
 		if len(id) == 0 {
-			id = uuid.NewV4().String()
+			uuid, err := uuid.NewV4()
+			if err != nil {
+				return fmt.Errorf("fail to generate UUID for X-Request-ID: %v", err)
+			}
+			id = uuid.String()
 			r.Header.Set("X-Request-ID", id)
 		}
 		r = r.WithContext(context.WithValue(r.Context(), "request_id", id))
