49e09e51296e7942a6375cb42eda8dae8e8f5072
Étienne Michon <etienne@scalingo.com>
2022-09-19 15:46:09 +0200

    docs(error_middleware): automatic comment formatting

--- error_middleware.go
+++ error_middleware.go
@@ -92,8 +92,8 @@ func writeError(log logrus.FieldLogger, w negroni.ResponseWriter, err error) {
 }
 
 // isContentTypeJSON returns true if the given string is a valid JSON value for the HTTP Content-Type header. Various values can be used to state that a payload is a JSON:
-// - The RFC 4627 defines the Content-Type "application/json" (https://datatracker.ietf.org/doc/html/rfc4627)
-// - The RFC 6839 defines the suffix "+json":
+//   - The RFC 4627 defines the Content-Type "application/json" (https://datatracker.ietf.org/doc/html/rfc4627)
+//   - The RFC 6839 defines the suffix "+json":
 //     The suffix "+json" MAY be used with any media type whose representation follows that established for "application/json"
 //     (https://datatracker.ietf.org/doc/html/rfc6839#page-4)
 func isContentTypeJSON(contentType string) bool {
