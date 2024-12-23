ab5c048b0f92fb5330f644f079845091108899da
Étienne Michon <etienne@scalingo.com>
2024-12-23 11:10:49 +0100

    build(go.mod): update 'github.com/urfave/negroni' from v2 to v3

--- error_middleware.go
+++ error_middleware.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 
 	"github.com/sirupsen/logrus"
-	"github.com/urfave/negroni/v2"
+	"github.com/urfave/negroni/v3"
 
 	"github.com/Scalingo/go-utils/errors/v2"
 	"github.com/Scalingo/go-utils/logger"
